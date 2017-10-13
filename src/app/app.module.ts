7ad6db04d3804ad8a14c55bae28110d9b76fc72d
Jhonnier Sanchez <jhonnier.sanchez@gmail.com>
2017-10-13 10:58:47 -0500

    google mpas - ngFor - ngFor - ngIf - *ngStyle - *ngClass - *ngSwitch

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAFhT8_Deob6i0WloYFQVhb__ytsLL0NfU'
+    })
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
